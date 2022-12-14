1d321349134685dea58e0c15586f94365afa411c
dkrol24 <krold2191@gmail.com>
2022-12-14 09:23:20 +0100

    /

--- utils/image.js
+++ utils/image.js
@@ -2,11 +2,11 @@ import ImageUrlBuilder from "@sanity/image-url";
 import client from "./client";
 
 function urlForThumbnail(source) {
-  return ImageUrlBuilder(client).image(source).width(800).url();
+  return ImageUrlBuilder(client).image(source).width(550).height(550).url();
 }
 
 function urlFor(source) {
-  return ImageUrlBuilder(client).image(source).width(800).url();
+  return ImageUrlBuilder(client).image(source).width(550).height(550).url();
 }
 
 export { urlFor, urlForThumbnail };
